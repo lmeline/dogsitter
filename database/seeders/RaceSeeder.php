762bef7f1252a65ef5252db868a79d80052e3333
Philippe Kurz <philippe.kurz@gmail.com>
2025-01-16 16:14:48 +0100

    ajout table races + seeder

---
+++ database/seeders/RaceSeeder.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class RaceSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $races = [
+            'Labrador Retriever',
+            'Berger Allemand',
+            'Golden Retriever',
+            'Bulldog',
+            'Beagle',
+            'Caniche',
+            'Chihuahua',
+            'Shih Tzu',
+            'Rottweiler',
+            'Bouledogue Français',
+            'Cocker Spaniel',
+            'Schnauzer',
+            'Yorkshire Terrier',
+            'Dachshund',
+            'Mastin Espagnol',
+            'Dalmatien',
+            'Border Collie',
+            'Bichon Frisé',
+            'Akita',
+            'Chow Chow',
+            'Husky Sibérien',
+            'Shiba Inu',
+            'Pitbull',
+            'Jack Russell Terrier',
+            'Pug',
+            'Weimaraner',
+            'Cavalier King Charles Spaniel',
+            'Lhassa Apso',
+            'Chesapeake Bay Retriever',
+            'Boxer',
+            'Great Dane',
+            'Saint-Bernard',
+            'Bernese Mountain Dog',
+            'English Setter',
+            'American Staffordshire Terrier',
+            'Newfoundland',
+            'Scottish Terrier',
+            'Whippet',
+            'Tibetan Mastiff',
+            'Vizsla',
+            'Shikoku',
+            'Papillon',
+            'American Bulldog',
+            'Basset Hound',
+            'Bloodhound',
+            'Saluki',
+            'Great Pyrenees',
+            'American Foxhound',
+            'Basenji',
+            'Belgian Malinois',
+            'Cairn Terrier',
+            'Gordon Setter',
+            'Dogo Argentino',
+            'Fox Terrier',
+            'Irish Setter',
+            'Norfolk Terrier',
+            'Pekingese',
+            'Schipperke',
+            'Tenterfield Terrier',
+            'Autre',
+        ];
+
+        foreach ($races as $race) {
+            DB::table('races')->insert([
+                'nom' => $race,
+            ]);
+        }
+    }
+}
