084ed54bcd6ffe54f9c7c491c21847a1a3e40eea
loise meline <loise13@hotmail.fr>
2025-02-27 16:48:50 +0100

    ajout api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\DogController;
 use App\Models\Dog;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
@@ -10,6 +11,4 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/dogs', function (Request $request) {
-    return response()->json(Dog::all());
-});
+Route::resource('/dogs',DogController::class);
