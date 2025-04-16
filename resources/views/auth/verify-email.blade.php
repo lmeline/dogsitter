bf4bd5625863ea94eec00770f1bcefb15a646e47
loise meline <loise13@hotmail.fr>
2025-04-16 14:51:05 +0200

    suppression de la page dashboard

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,5 +1,5 @@
 <x-guest-layout>
-    <div class="mb-4 text-sm text-black-600 dark:text-black-400">
+    <div class="mb-4 p-5 text-sm text-black-600 dark:text-black-400">
         {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
     </div>
 
