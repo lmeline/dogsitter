d32b7ceb4fe47573ba536966773087afe02a67c6
Loïse Méline <loise13@hotmail.fr>
2025-04-30 20:49:07 +0200

    mise en place dun seul formulaire

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -36,6 +36,7 @@ class RegisteredUserController extends Controller
      */
     public function store(Request $request): RedirectResponse
     {
+        //dd($request->all());
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'prenom' => ['required', 'string', 'max:70'],
@@ -46,7 +47,8 @@ class RegisteredUserController extends Controller
             'ville_id' => ['required', 'exists:villes,id'],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
             'date_naissance' => ['required', 'date', 'before:18 year ago'],
-            'photo' => ['nullable', 'string', 'max:255']
+            'photo' => ['nullable', 'string', 'max:255'],
+            'role' =>['required', 'string', 'max:255']
         ]);
 
         $user = User::create([
@@ -59,67 +61,19 @@ class RegisteredUserController extends Controller
             'code_postal' => $request->code_postal,
             'ville_id' => $request->ville_id,
             'password' => Hash::make($request->password),
-            'photo' => $request->photo
+            'photo' => $request->photo,
+            'role' => $request->role
         ]);
 
         event(new Registered($user));
 
         Auth::login($user);
 
-        return redirect()->intended(route('register.dog'));
+        if($request->role == 'proprietaire') {
+            return redirect()->route('register.dog');
+        } else {
+            return redirect()->route('register.abonnement');
+        }
     }
 
-    public function storedogsitter(Request $request)
-    {
-            $request->validate([
-                'name' => ['required', 'string', 'max:255'],
-                'prenom' => ['required', 'string', 'max:70'],
-                'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:' . User::class],
-                'telephone' => ['required', 'string', 'max:15'],
-                'adresse' => ['required', 'string', 'max:255'],
-                'code_postal' => ['required', 'string', 'max:20'],
-                'ville_id' => ['required', 'exists:villes,id'],
-                'password' => ['required', 'confirmed', Rules\Password::defaults()],
-                'date_naissance' => ['required', 'date', 'before:18 year ago'],
-                'description' => ['nullable', 'string', 'max:255'],
-                'photo' => ['nullable', 'image', 'mimes:jpeg,png,jpg,gif', 'max:2048']
-    
-            ]);
-    
-            $role = 'dogsitter';
-    
-            if ($request->hasFile('photo')) {
-                $photoPath = $request->file('photo')->store('photos', 'public');
-            } else {
-                $photoPath = null; 
-            }
-    
-            $user = User::create([
-                'role' => $role,
-                'name' => $request->name,
-                'prenom' => $request->prenom,
-                'email' => $request->email,
-                'numero_telephone' => $request->telephone,
-                'adresse' => $request->adresse,
-                'date_naissance' => $request->date_naissance,
-                'code_postal' => $request->code_postal,
-                'ville_id' => $request->ville_id,
-                'password' => Hash::make($request->password),
-                'description' => $request->description,
-                'photo' => $photoPath
-            ]);
-    
-            if ($request->hasFile('photo')) {
-                Storage::disk('public')->delete($user->photo); 
-                $user->photo = $request->file('photo')->store('photos', 'public');
-                $user->save();
-            }
-            
-            event(new Registered($user));
-    
-            Auth::login($user);
-    
-            return redirect()->intended(route('register.abonnement'));
-    
-    }
 }
