599ab19c89d9546a759191f86f81f3d640bfc5de
loise meline <loise13@hotmail.fr>
2025-05-22 09:48:23 +0200

    changement de la taille de la croix pour les messageries

--- resources/views/messages/index.blade.php
+++ resources/views/messages/index.blade.php
@@ -12,15 +12,12 @@
         <div id="searchModal" class="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50 hidden">
             <div class="bg-white rounded-lg shadow-lg w-full max-w-lg h-96 flex flex-col">
                 @if (Auth::user()->role === 'dogsitter')
-                <!-- Conteneur du haut (fixe) -->
                     <div class="p-4 flex justify-between items-center">
                         <h2 class="text-xl font-semibold">Rechercher un propriétaire</h2>
-                        <button onclick="toggleModal()" class="text-gray-600 hover:text-gray-900 text-2xl"><svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 32 32">
+                        <button onclick="toggleModal()" class="text-gray-600 hover:text-gray-900 text-2xl"><svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="25" height="25" viewBox="0 0 32 32">
                             <path d="M 16 3 C 8.832031 3 3 8.832031 3 16 C 3 23.167969 8.832031 29 16 29 C 23.167969 29 29 23.167969 29 16 C 29 8.832031 23.167969 3 16 3 Z M 16 5 C 22.085938 5 27 9.914063 27 16 C 27 22.085938 22.085938 27 16 27 C 9.914063 27 5 22.085938 5 16 C 5 9.914063 9.914063 5 16 5 Z M 12.21875 10.78125 L 10.78125 12.21875 L 14.5625 16 L 10.78125 19.78125 L 12.21875 21.21875 L 16 17.4375 L 19.78125 21.21875 L 21.21875 19.78125 L 17.4375 16 L 21.21875 12.21875 L 19.78125 10.78125 L 16 14.5625 Z"></path>
                             </svg></button>
                     </div>
-            
-                    <!-- Barre de recherche (fixe) -->
                     <div class="p-4">
                         <input type="text" id="searchInput" placeholder="Nom du propriétaire..."
                             class="w-full border border-gray-300 rounded-lg shadow-md">
@@ -28,21 +25,17 @@
                 @else
                     <div class="p-4 flex justify-between items-center ">
                         <h2 class="text-xl font-semibold">Rechercher un dogsitter</h2>
-                        <button onclick="toggleModal()" class="text-gray-600 hover:text-gray-900 text-2xl"><svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="30" height="30" viewBox="0 0 32 32">
+                        <button onclick="toggleModal()" class="text-gray-600 hover:text-gray-900 text-2xl"><svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="25" height="25" viewBox="0 0 32 32">
                             <path d="M 16 3 C 8.832031 3 3 8.832031 3 16 C 3 23.167969 8.832031 29 16 29 C 23.167969 29 29 23.167969 29 16 C 29 8.832031 23.167969 3 16 3 Z M 16 5 C 22.085938 5 27 9.914063 27 16 C 27 22.085938 22.085938 27 16 27 C 9.914063 27 5 22.085938 5 16 C 5 9.914063 9.914063 5 16 5 Z M 12.21875 10.78125 L 10.78125 12.21875 L 14.5625 16 L 10.78125 19.78125 L 12.21875 21.21875 L 16 17.4375 L 19.78125 21.21875 L 21.21875 19.78125 L 17.4375 16 L 21.21875 12.21875 L 19.78125 10.78125 L 16 14.5625 Z"></path>
                             </svg></button>
                     </div>
             
-                    <!-- Barre de recherche (fixe) -->
                     <div class="p-4">
                         <input type="text" id="searchInput2" placeholder="Nom du dogsitter..."
                             class="w-full border border-gray-300 rounded-lg shadow-md">
                     </div>
                 @endif
-
-                <!-- Conteneur des résultats (défilable) -->
                 <div id="searchResults" class="flex-1 overflow-y-auto p-4">
-                    <!-- Les propriétaires apparaîtront ici -->
                 </div>
         
             </div>
